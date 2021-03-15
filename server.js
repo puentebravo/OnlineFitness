3416eb424e0046b38f7d4deaff3eb15de1aa603a
Puentebravo <puentematos@gmail.com>
2021-03-14 23:23:12 -0400

    fixed server route connections

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 });
 
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
