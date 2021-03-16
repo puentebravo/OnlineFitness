6fd980c644fa53270b83106b2ade5fbbe3c96105
Puentebravo <puentematos@gmail.com>
2021-03-16 01:01:08 -0400

    Combined models into single workout model, renamed resistance.js

---
+++ models/workout.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Please enter a workout name.",
+  },
+  weight: {
+    type: Number,
+    trim: true,
+  },
+  sets: {
+    type: Number,
+    trim: true,
+  },
+  reps: {
+    type: Number,
+    trim: true,
+  },
+  distance: {
+    type: Number,
+    trim: true
+  },
+  duration: {
+    type: Number,
+    trim: true,
+    required: "Please enter a workout duration.",
+  },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
