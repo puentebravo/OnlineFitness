0e9523b29255e09b34de10b1640d7312a032e825
Puentebravo <puentematos@gmail.com>
2021-03-17 00:56:01 -0400

    Added get routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,16 @@
 const router = require("express").Router();
+const Workout = require("../models/workout.js");
 
+router.get("/api/workouts", (req, res) => {
+  Workout.find({}).then((data) => {
+    res.json(data);
+  });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({ $limit: 7 })
+    .sort({ date: -1 })
+    .then((workout) => {
+      res.json(workout);
+    });
+});
