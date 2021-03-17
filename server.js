725812e09d3a691faf477b0eba26c4e833451ebe
Puentebravo <puentematos@gmail.com>
2021-03-17 00:12:50 -0400

    Added MONGODB atlas code to server

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ require("./routes/htmlRoutes.js")(app);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 
