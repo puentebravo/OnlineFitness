361a8adf0314fa363ab18d7ae15ee87d5724cddb
Puentebravo <puentematos@gmail.com>
2021-03-18 17:27:00 -0400

    Replaced find with aggregate functions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,35 @@
-// const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
 module.exports = (app) => {
   app.get("/api/workouts", (req, res) => {
-    Workout.find({}).then((data) => {
+    Workout.aggregate([
+      {
+        $addFields: {
+          totalWeight: { $sum: "$weight" },
+          totalDuration: { $sum: "$duration" },
+        },
+      },
+    ]).then((data) => {
       res.json(data);
     });
   });
 
   app.get("/api/workouts/range", (req, res) => {
-    Workout.find({})
-      .sort({ date: -1 })
-      .then((workout) => {
-        res.json(workout);
-      });
+    Workout.aggregate([
+      {
+        $addFields: {
+          totalWeight: { $sum: "$weight" },
+          totalDuration: { $sum: "$duration" },
+        },
+      },
+      {
+        $limit: 7,
+      },
+      {
+        $sort: { day: -1, _id: -1 },
+      },
+    ]).then((workout) => {
+      res.json(workout);
+    });
   });
 };
