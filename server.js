abe79bcb87de1d7765afc746d019c475edf926d8
Puentebravo <puentematos@gmail.com>
2021-03-18 00:43:34 -0400

    Fixed error linking to api routes file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -10,10 +10,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
